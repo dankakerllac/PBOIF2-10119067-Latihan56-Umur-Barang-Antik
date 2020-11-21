7bec184d847c2291cf55ab510e9d288dc51e37cb
User <User@DESKTOP-6N6EHH6>
2020-11-21 22:48:19 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan56/umur/barang/antik/Radio.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067.latihan56.umur.barang.antik;
+
+/**
+ *
+ * @author Danhago
+ * NAMA     : Andreas Danhago P.I
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi: Barang Antik
+ * 
+ */
+public class Radio extends BarangAntik{
+    private String name;
+
+    public Radio(int umur) {
+        super(umur);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
